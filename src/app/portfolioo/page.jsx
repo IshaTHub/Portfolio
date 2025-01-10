8f779903c41aa68be9c165e9338f38c46da1ab8f
IshaT-Hub <ishatiwari0508@gmail.com>
2025-01-10 18:08:23 +0530

    new

--- src/app/portfolioo/page.jsx
+++ src/app/portfolioo/page.jsx
@@ -1,6 +1,14 @@
+"use client";
+import { motion } from "framer-motion";
+
 const Portfolioo = () =>{
     return (
-        <div>Portfolio Page</div>
+        <motion.div className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+        Portfolio Page</motion.div>
     )
 }
 
